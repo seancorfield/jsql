4f5524ae15d7622b0293510b3c4af1d5c986cea3
Sean Corfield <sean@worldsingles.com>
2012-06-16 20:16:05 -0700

    Started documentation. Added delete function. Reorganize code.

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject jsql "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Experimental DSL for SQL generation for next version of clojure.java.jdbc."
+  :url "https://github.com/seancorfield/jsql"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  ;; currently relying on experimental as-identifier functionality in clojure.java.jdbc
   :repositories [["sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]]
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/java.jdbc "0.2.3-SNAPSHOT"]])
--- src/jsql/core.clj
+++ src/jsql/core.clj
@@ -3,16 +3,11 @@
             [clojure.string :refer [join] :rename {join str-join}]
             [clojure.walk :refer [postwalk]]))
 
-(defn where [param-map & {:keys [entity] :or {entity identity}}]
-  (let [ks (keys param-map)
-        vs (vals param-map)]
-    (cons
-     (str-join
-      " AND "
-      (map (fn [k v]
-             (str (jdbc/as-identifier k entity) (if (nil? v) " IS NULL" " = ?")))
-           ks vs))
-     (remove nil? vs))))
+(defn- col-str [col entity]
+  (if (map? col)
+    (let [[k v] (first col)]
+      (str (jdbc/as-identifier k entity) " AS " (jdbc/as-identifier v entity)))
+    (jdbc/as-identifier col entity)))
 
 (defn- table-str [table entity]
   (if (map? table)
@@ -20,11 +15,24 @@
       (str (jdbc/as-identifier k entity) " " (jdbc/as-identifier v entity)))
     (jdbc/as-identifier table entity)))
 
-(defn- col-str [col entity]
-  (if (map? col)
-    (let [[k v] (first col)]
-      (str (jdbc/as-identifier k entity) " AS " (jdbc/as-identifier v entity)))
-    (jdbc/as-identifier col entity)))
+(def ^:private sql-syms
+  #{"delete" "insert" "join" "select" "update" "where"})
+
+(defmacro naming [entity sql]
+  (postwalk (fn [form]
+              (if (and (seq? form)
+                       (symbol? (first form))
+                       (sql-syms (name (first form))))
+                (concat form [:entity entity])
+                form)) sql))
+
+(defn delete [table [where & params] & {:keys [entity] :or {entity identity}}]
+  (into [(str "DELETE FROM " (table-str table entity)
+              (when where " WHERE ") where)]
+        params))
+
+(defn insert [tables & clauses]
+  [""])
 
 (defn join [table on-map & {:keys [entity] :or {entity identity}}]
   (str " JOIN " (table-str table entity) " ON "
@@ -39,16 +47,19 @@
                            (first where-etc))
         options (if where (rest where-etc) where-etc)
         {:keys [entity] :or {entity identity}} (apply hash-map  options)]
-    [(str
-      "SELECT "
-      (if (= * col-seq)
-        "*"
-        (str-join "," (map #(col-str % entity) col-seq)))
-      " FROM " (table-str table entity)
-      (str-join " " joins)
-      (when where " WHERE ")
-      where)
-     params]))
+    (into [(str
+            "SELECT "
+            (cond
+             (= * col-seq) "*"
+             (or (string? col-seq)
+                 (keyword? col-seq)
+                 (map? col-seq)) (col-str col-seq entity)
+             :else (str-join "," (map #(col-str % entity) col-seq)))
+            " FROM " (table-str table entity)
+            (str-join " " joins)
+            (when where " WHERE ")
+            where)]
+          params)))
 
 (defn update [table set-map & where-etc]
   (let [[where-clause & options] (when-not (keyword? (first where-etc)) where-etc)
@@ -56,25 +67,25 @@
         {:keys [entity] :or {entity identity}} (if (keyword? (first where-etc)) where-etc options)
         ks (keys set-map)
         vs (vals set-map)]
-    [(str "UPDATE " (table-str table entity)
-          " SET " (str-join
-                   ","
-                   (map (fn [k v] (str (jdbc/as-identifier k entity)
-                                      " = "
-                                      (if (nil? v) "NULL" "?")))
-                        ks vs))
-          (when where " WHERE ")
-          where
-          )
-     (concat (remove nil? vs) (rest where-clause))]))
+    (into [(str "UPDATE " (table-str table entity)
+                " SET " (str-join
+                         ","
+                         (map (fn [k v] (str (jdbc/as-identifier k entity)
+                                            " = "
+                                            (if (nil? v) "NULL" "?")))
+                              ks vs))
+                (when where " WHERE ")
+                where
+                )]
+          (concat (remove nil? vs) params))))
 
-(def ^:private sql-syms
-  #{"insert" "join" "select" "update" "where"})
-
-(defmacro naming [entity sql]
-  (postwalk (fn [form]
-              (if (and (seq? form)
-                       (symbol? (first form))
-                       (sql-syms (name (first form))))
-                (concat form [:entity entity])
-                form)) sql))
+(defn where [param-map & {:keys [entity] :or {entity identity}}]
+  (let [ks (keys param-map)
+        vs (vals param-map)]
+    (cons
+     (str-join
+      " AND "
+      (map (fn [k v]
+             (str (jdbc/as-identifier k entity) (if (nil? v) " IS NULL" " = ?")))
+           ks vs))
+     (remove nil? vs))))
