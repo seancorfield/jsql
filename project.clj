11a7efb534a4f5e138957c0c1e3a5c585b0a55d1
Sean Corfield <sean@worldsingles.com>
2012-06-22 15:40:28 -0700

    Bump to released 0.2.3 for jdbc

--- project.clj
+++ project.clj
@@ -3,8 +3,6 @@
   :url "https://github.com/seancorfield/jsql"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  ;; currently relying on experimental as-identifier functionality in clojure.java.jdbc
-  :repositories [["sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]]
   :dev-dependencies [[lein-autoexpect "0.1.1"]
                      [lein-expectations "0.0.3"]
                      [expectations "1.4.3"]
@@ -12,4 +10,4 @@
                      [swank-clojure "1.4.2"]
                      [mysql/mysql-connector-java "5.1.6"]]
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/java.jdbc "0.2.3-SNAPSHOT"]])
+                 [org.clojure/java.jdbc "0.2.3"]])
