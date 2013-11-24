6e0475ad0fd5c55c1099f5645ae1918fdd9f9195
Sean Corfield <sean@worldsingles.com>
2013-11-23 17:03:32 -0800

    Add description / URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject java-jdbc/dsl "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Basic DSL for generating SQL/DDL formerly part of org.clojure/java.jdbc"
+  :url "https://github.com/seancorfield/jsql"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
