522b115051811876faf18ac7bc110dd2458a1fc0
Sean Corfield <sean@corfield.org>
2014-11-27 10:01:05 -0800

    Start 0.1.2 stream

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject java-jdbc/dsl "0.1.1"
+(defproject java-jdbc/dsl "0.1.2-SNAPSHOT"
   :description "Basic DSL for generating SQL/DDL formerly part of org.clojure/java.jdbc"
   :url "https://github.com/seancorfield/jsql"
   :license {:name "Eclipse Public License"
